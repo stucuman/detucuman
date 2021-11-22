ef19e6b52c0e6c74a52d6db4d938f40fe65662ac
Seba Gomes <sebastian.gomes@gmail.com>
2021-11-22 19:49:44 -0300

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,51 +6,23 @@ const Home = () => (
       <title>Next.js on the Jamstack with Netlify!</title>
     </Head>
     <h1>
-      Next.js on the <a href="https://jamstack.org">Jamstack</a>
+      mira la  <a href="https://lacritica.com.ar/quiniela-de-tucuman">Quiniela de Tucuman</a>
     </h1>
+  <h1>
+      Mira la  <a href="https://lacritica.com.ar/quiniela-nacional">quiniela nacional</a>
+    </h1>
+
+  <h1>
+      Conoce <a href="https://lacritica.com.ar/quiniela">quiniela enzo</a>
+    </h1>
+
 
     <h3>
       Hooray 🎉 - you've built this with{' '}
       <a href="https://nextjs.org">Next.js</a>!
   
     </h3>
-  <div><h1><a href="https://lacritica.com.ar/?s=daniela+orce%C2%A0">daniela orce</a></h1></div>  
- <div><h1><a href="https://lacritica.com.ar/?s=lucia+rossi%C2%A0">lucia rossi</a></h1></div>  
- <div><h1><a href="http://argencon.org">Argencon</a></h1></div>  
- <div><h1><a href="http://telefonosatencioncliente.xyz">Telefonos de atencion al cliente</a></h1></div>  
- <div><h1><a href="https://lacritica.netlify.app">netlify</a></h1></div>  
- <div><h1><a href="https://sites.google.com/view/tuquman/">tucuman  
- <div><h1><a href="https://sebastiangomes5.wixsite.com/tuquman">tucuman  
- <div><h1><a href="https://proyectoasistir.com.ar">piscologo online</a></h1></div>  
- <div><h1><a href="https://sastreriagiordano.com.ar">alquiler de trajes en tucuman</a></h1></div>  
- <div><h1><a href="https://proyectoasistir.com.ar">piscologo online </a></h1></div>  
- <div><h1><a href="https://sastreriagiordano.com.ar">alquiler de trajes para fiestas en tucuman</a></h1></div>  
- <div><h1><a href="https://sastreriagiordano.com.ar">alquiler de trajes en tucuman</a></h1></div>  
- <div><h1><a href="https://quepicada.com.ar">picadas en Tucuman</a></h1></div>  
- <div><h1><a href="https://quepicada.com.ar">delivery de picadas en Tucuman</a></h1></div>  
- <div><h1><a href="https://quepicada.com.ar">picadas a domicilio en Tucuman</a></h1></div>  
- <div><h1><a href="https://quepicada.com.ar">picadas para regalar en Tucuman</a></h1></div>  
- <div><h1><a href="https://quepicada.com.ar">regalos empresariales en Tucuman</a></h1></div>  
- <div><h1><a href="https://quepicada.com.ar">box para regalar en tucuman</a></h1></div>  
- <div><h1><a href="https://images.google.de/url?q=https://lacritica.com.ar/">noticias de tucuman</a></h1></div>
- <div><h1><a href="https://images.google.co.uk/url?q=https://lacritica.com.ar/">noticias de tucuman</a></h1></div>
- <div><h1><a href="https://images.google.co.jp/url?q=https://lacritica.com.ar/">tucuman</a></h1></div>
- <div><h1><a href="https://images.google.co.jp/url?q=https://proyectoasistir.com.ar">piscologo online</a></h1></div>  
- <div><h1><a href="https://images.google.es/url?q=https://lacritica.com.ar/">ultimo momento tucuman</a></h1></div>
- <div><h1><a href="https://www.ted.com/profiles/28003350">TED  </a></h1></div>
- 
- <div><h1><a href="https://people.mozilla.org/p/sebasg">Mozilla</a></h1></div>
- 
- <div><h1><a href="https://www.google.es/url?q=https://lacritica.com.ar/"> ricardo bussi </a></h1></div>  
- <div><h1><a href="https://spark.adobe.com/page/QGyPFWsmiJCTK/">adobe </a></h1></div>  
- <div><h1><a href="marketingdigitalexperience.com/curso-de-marketing-digital/">curso de marketing digital </a></h1></div>  
- <div><h1><a href="https://huffduffer.com/sebasg/610279">huffduffer </a></h1></div> 
- <div><h1><a href="https://spacebabe.ams3.digitaloceanspaces.com/index.html">Spaces babe</a></h1></div>  
- <div><h1><a href="https://epaepa.oss-ap-northeast-1.aliyuncs.com/index.html">Epaepadiscoestepa</a></h1></div>  
- <div><h1><a href="https://bit.ly/3eLLr0v">bitly</a></h1></div>  
- <div><h1><a href="https://tucuman.hatenablog.com/entry/2021/05/15/133138">hatena</a></h1></div>  
- <div><h1><a href="https://lacritica.com.ar/2021/05/16/barbie-beltramino/">Barbie Beltramino</a></h1></div>  
- <div><h1><a href="https://ar.pinterest.com/LACRITICAok">pinterest</a></h1></div>  
+
 
     <style jsx>{`
       :global(html, body) {
